97aefa936e93b183f5dfde1a3f865bac1071b234
anthonypenapalomino8-prog <anthonypenapalomino8@gmail.com>
2025-12-29 21:12:52 -0500

    Update juego3.js

--- juegos/juego3.js
+++ juegos/juego3.js
@@ -1,17 +1,8 @@
-let t;
-const b = document.getElementById("hold");
-
-b.addEventListener("mousedown",start);
-b.addEventListener("touchstart",start);
-b.addEventListener("mouseup",stop);
-b.addEventListener("touchend",stop);
-
-function start(){
-  t=setTimeout(()=>{
-    location.href="../cartas/carta3.html";
-  },4000);
-}
-
-function stop(){
-  clearTimeout(t);
+function verificar() {
+  const v = document.getElementById("input").value.toLowerCase();
+  if (v === "esperar") {
+    location.href = "../cartas/carta3.html";
+  } else {
+    alert("No es esa palabra… piensa con el corazón.");
+  }
 }
