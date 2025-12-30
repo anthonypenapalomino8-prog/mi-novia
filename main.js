bc6a9bef4dfd588426a34c6325b3551ff9af2488
anthonypenapalomino8-prog <anthonypenapalomino8@gmail.com>
2025-12-29 20:52:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-function irJuego() {
-  const juegos = [
-    "juegos/juego1.html",
-    "juegos/juego2.html",
-    "juegos/juego3.html"
-  ];
-  const r = Math.floor(Math.random() * juegos.length);
-  window.location.href = juegos[r];
+function irJuego(n) {
+  document.body.classList.add("fade-out");
+  setTimeout(() => {
+    window.location.href = `juegos/juego${n}.html`;
+  }, 500);
 }
