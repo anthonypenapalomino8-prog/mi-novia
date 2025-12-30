2a3a9d024d513bcbd11e0ccd6d5129a22641ef94
anthonypenapalomino8-prog <anthonypenapalomino8@gmail.com>
2025-12-29 21:05:49 -0500

    Update juego3.js

--- juegos/juego3.js
+++ juegos/juego3.js
@@ -1,24 +1,17 @@
-const circle = document.getElementById("circle");
-let timer;
+let t;
+const b = document.getElementById("hold");
 
-circle.addEventListener("touchstart", start);
-circle.addEventListener("mousedown", start);
+b.addEventListener("mousedown",start);
+b.addEventListener("touchstart",start);
+b.addEventListener("mouseup",stop);
+b.addEventListener("touchend",stop);
 
-circle.addEventListener("touchend", stop);
-circle.addEventListener("mouseup", stop);
-circle.addEventListener("mouseleave", stop);
-
-function start() {
-  circle.style.background = "rgba(255,255,255,0.5)";
-  timer = setTimeout(() => {
-    circle.innerText = "Gracias";
-    setTimeout(() => {
-      window.location.href = "../cartas/carta3.html";
-    }, 1000);
-  }, 3000);
+function start(){
+  t=setTimeout(()=>{
+    location.href="../cartas/carta3.html";
+  },4000);
 }
 
-function stop() {
-  clearTimeout(timer);
-  circle.style.background = "rgba(255,255,255,0.2)";
+function stop(){
+  clearTimeout(t);
 }
