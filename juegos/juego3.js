1e76d81e6e9286046979d5806e9a9c0d9b97679d
anthonypenapalomino8-prog <anthonypenapalomino8@gmail.com>
2025-12-29 20:41:44 -0500

    Update juego3.js

--- juegos/juego3.js
+++ juegos/juego3.js
@@ -1,5 +1,24 @@
-function ganar() {
-  window.location.href = "../cartas/carta3.html";
+const circle = document.getElementById("circle");
+let timer;
+
+circle.addEventListener("touchstart", start);
+circle.addEventListener("mousedown", start);
+
+circle.addEventListener("touchend", stop);
+circle.addEventListener("mouseup", stop);
+circle.addEventListener("mouseleave", stop);
+
+function start() {
+  circle.style.background = "rgba(255,255,255,0.5)";
+  timer = setTimeout(() => {
+    circle.innerText = "Gracias";
+    setTimeout(() => {
+      window.location.href = "../cartas/carta3.html";
+    }, 1000);
+  }, 3000);
 }
 
-
+function stop() {
+  clearTimeout(timer);
+  circle.style.background = "rgba(255,255,255,0.2)";
+}
